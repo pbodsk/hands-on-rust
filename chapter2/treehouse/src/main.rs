5c58a86e7eac87cca355243a0103087f755083bb
Peter Bødskov <me@peterbodskov.dk>
2020-12-24 12:08:58 +0100

    chapter 2 cont

--- chapter2/treehouse/src/main.rs
+++ chapter2/treehouse/src/main.rs
@@ -1,9 +1,21 @@
 use std::io::stdin;
 
-fn main() {
-    println!("Hello, what's your name?");
-    let name = what_is_your_name();
-    println!("Hello {}", name);
+struct Visitor {
+    name: String,
+    greeting: String
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string()
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
 }
 
 fn what_is_your_name() -> String {
@@ -14,4 +26,24 @@ fn what_is_your_name() -> String {
     your_name
         .trim()
         .to_lowercase()
+}
+
+fn main() {
+    let visitor_list = [
+        Visitor::new("Ask", "Hello there Ask"),
+        Visitor::new("Freja", "Welcome Freja"),
+        Visitor::new("Pia", "Hi Pia"),
+        Visitor::new("Peter", "Good day Peter")
+    ];
+    println!("Hello, what's your name?");
+    let name = what_is_your_name();
+
+    let known_visitor = visitor_list
+        .iter()
+        .find(|visitor| visitor.name == name);
+
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("Hey {}, no access for you buddy", name)
+    }    
 }
