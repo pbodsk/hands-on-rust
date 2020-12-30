22548799a546cd3b5adf0896d0aad26599b3ca27
Peter Bødskov <me@peterbodskov.dk>
2020-12-30 18:39:18 +0100

    more work on chapter two

--- chapter2/treehouse/src/main.rs
+++ chapter2/treehouse/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::stdin;
 
+#[derive(Debug)]
 struct Visitor {
     name: String,
     greeting: String
@@ -29,21 +30,35 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    let visitor_list = [
+    let mut visitor_list = vec![
         Visitor::new("Ask", "Hello there Ask"),
         Visitor::new("Freja", "Welcome Freja"),
         Visitor::new("Pia", "Hi Pia"),
         Visitor::new("Peter", "Good day Peter")
     ];
-    println!("Hello, what's your name?");
-    let name = what_is_your_name();
 
-    let known_visitor = visitor_list
-        .iter()
-        .find(|visitor| visitor.name == name);
+    loop {
+        println!("Hello, what's your name? (leave empty and press ENTER to quit)");
+        let name = what_is_your_name();
 
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        None => println!("Hey {}, no access for you buddy", name)
-    }    
+        let known_visitor = visitor_list
+            .iter()
+            .find(|visitor| visitor.name == name);
+    
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("Hey {}, no access for you buddy", name);
+                    let mut greeting = String::from("Welcome ");
+                    greeting.push_str(&name);
+                    visitor_list.push(Visitor::new(&name, &greeting));
+                }
+            }
+        }           
+    }   
+    println!("The final list of visitors");
+    println!("{:#?}", visitor_list);
 }
