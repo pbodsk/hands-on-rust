77465a22c5e7c259fbb8371af36469e8072040ad
Peter Bødskov <me@peterbodskov.dk>
2021-02-07 17:18:31 +0100

    chapter 7 done

--- dungeoncrawl/src/spawner.rs
+++ dungeoncrawl/src/spawner.rs
@@ -33,7 +33,8 @@ pub fn spawn_monster(
                     2 => to_cp437('o'),
                     _ => to_cp437('g'),
                 }
-            }
+            },
+            MovingRandomly{}
         )
     );
 }
--- dungeoncrawl/src/systems/player_input.rs
+++ dungeoncrawl/src/systems/player_input.rs
@@ -5,10 +5,13 @@ use crate::prelude::*;
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld, 
-    #[resource] map: &Map, 
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera
+    #[resource] turn_state: &mut TurnState
 ) {
+    let mut players = <(Entity, &Point)>::query()
+        .filter(component::<Player>());
+
     if let Some(key) = key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
@@ -18,17 +21,12 @@ pub fn player_input(
             _ => Point::new(0, 0),
         };
 
-        // did we move?
-        if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query()
-                .filter(component::<Player>());
-            players.iter_mut(ecs).for_each( |pos| {
-                let destination = *pos + delta;
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                }
-            });
-        }
+        players.iter_mut(ecs).for_each( |(entity, pos) | {
+            let destination = *pos + delta;
+            commands.push(((), WantsToMove{
+                entity: *entity, destination
+            }));
+        });
+        *turn_state = TurnState::PlayerTurn;            
     }
 }
